87ea3bee52a63cf0bd1bceb03468486b9d02cebe
Andrey Martynov <49904130+CloudHunterTHEreal@users.noreply.github.com>
2024-05-29 10:13:24 +0300

    Short main script logic comments storing

---
+++ tmptask.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/etc bash
+
+echo "Start study script"
+
+#tst_cmd="ls -lh $1"
+
+#echo "Output $tst_cmd is:"
+#echo "$($tst_cmd)"
+
+# Load script config
+#
+# Request control dir info
+#
+# Pop prev dir info                                     #
+# Log current dur unto file                             # Push current dir info
+#
+# Free space size dynamic calculation
+#
+# Case script run mode:
+#     guess) Drop enougth oldest file for suport setted free space until next control script run
+#     lazy) Goal - free space
